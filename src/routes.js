68d8e1c945cd7bd2703a2a5b3aa597c79dc3aad2
Augusto Corvalan <augustocorvalan@gmail.com>
2016-09-07 18:55:33 -0400

    remove accidental duplicate Providers

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import { Provider } from 'react-redux';
 import store from './store';
 
 //components
@@ -8,9 +7,7 @@ import App from './common/components/App';
 import HomePage from './pages/home/page';
 
 export default (
-	<Provider store={store} >
-		<Route path="/" component={App}>
-			<IndexRoute component={HomePage} />
-		</Route>
-	</Provider>
+	<Route path="/" component={App}>
+		<IndexRoute component={HomePage} />
+	</Route>
 );
