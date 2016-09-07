53dbdcf7b767612b8d2380a393d3b1aa8b01923c
Augusto Corvalan <augustocorvalan@gmail.com>
2016-09-07 18:57:04 -0400

    remove App comp from home page

--- src/pages/home/page.js
+++ src/pages/home/page.js
@@ -2,16 +2,10 @@ import React from "react";
 import { connect } from 'react-redux';
 import styles from "./style.css";
 
-//components
-import App from "../../common/components/App";
-
 class HomePage extends React.Component {
 	render() {
-
 		return (
-			<App>
-				<h1>Welcome to Redux Shell</h1>
-			</App>
+			<h1>Welcome to Redux Shell</h1>
 		);
 	}
 };
