a74f3fb6531f9b4167c6b3077928507e269ee268
Augusto Corvalan <augustocorvalan@gmail.com>
2016-09-13 17:11:16 -0400

    inject production env var

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -13,6 +13,11 @@ module.exports = {
   },
 
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
     new ExtractTextPlugin('style.css', { allChunks: true })
   ],
 
